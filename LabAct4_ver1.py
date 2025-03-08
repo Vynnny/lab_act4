bbe1908511ca50bdd2160bf5a2af5b2dd4896a98
vynnny <diego.b.23456@gmail.com>
2025-03-08 15:56:14 +0800

    Add files via upload

---
+++ LabAct4_ver1.py
@@ -0,0 +1,20 @@
+#Lab Act 4:
+	#ver 1:
+
+marks = int(input("say grade:"))
+if marks <= 0:
+	print("invalid")
+if marks >= 101:
+	print("invalid2")
+elif marks >= 90:
+	print("Grade: A")
+elif marks >= 80:
+	print("Grade: B")
+elif marks >= 70:
+	print("Grade: C")
+elif marks >= 69:
+	print("Grade: D")
+else:
+	print("Grade: F")
+
+#Jose Diego
